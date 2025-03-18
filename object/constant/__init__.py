a7c2be362166ba5f04d894134973a4afbb7a8f12
RisAhamed <riswanahamed38@gmail.com>
2025-03-18 07:06:40 +0530

    Roboflow annotated Data Uploaded to S3 and data ingestion completed successfully

--- object/constant/__init__.py
+++ object/constant/__init__.py
@@ -4,7 +4,7 @@ ARTIFACTS_DIR: str = "artifacts"
 
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
-DATA_INGESTION_S3_DATA_NAME: str = "image.zip"
+DATA_INGESTION_S3_DATA_NAME: str = "RoboflowImages.zip"
 DATA_INGESTION_S3_BUCKET_NAME: str = "mlops-object-data"
 
 """DATA validation realated Constants"""
